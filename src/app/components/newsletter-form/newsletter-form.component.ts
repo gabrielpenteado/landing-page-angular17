1ab10a0ef69d5adff69d5dea6abae7263a08b618
Gabriel Penteado <gabripenteado@gmail.com>
2024-05-06 16:51:28 -0300

    created form

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, signal } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+
+@Component({
+  selector: 'newsletter-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss',
+})
+export class NewsletterFormComponent {
+  newsLetterForm!: FormGroup;
+  loading = signal(false);
+
+  constructor(private service: NewsletterService) {
+    this.newsLetterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    this.loading.set(true);
+    if (this.newsLetterForm.valid) {
+      this.service
+        .sendData(
+          this.newsLetterForm.value.name,
+          this.newsLetterForm.value.email
+        )
+        .subscribe({
+          next: () => {
+            this.newsLetterForm.reset();
+            this.loading.set(false);
+          },
+        });
+    }
+  }
+}
